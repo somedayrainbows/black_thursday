8423f80a20821d57a1a8a8c0d14ac83f5fc45476
somedayrainbows <ebassity@gmail.com>
2017-02-28 15:05:15 -0700

    finished invoice_item_repo and updated repo_methods

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -1,6 +1,7 @@
 require_relative './../lib/invoice_item_repository'
 require 'csv'
 require_relative 'test_helper'
+require 'pry'
 
 class InvoiceItemRepositoryTest < Minitest::Test
   attr_reader :se, :invoice_item_repository
@@ -18,6 +19,30 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 467, se.invoice_items.all.count
     assert Array, invoice_item_repository.all
     assert_instance_of InvoiceItem, invoice_item_repository.all.sample
+  end
+
+  def test_it_can_find_itself_with_its_id
+    assert_instance_of InvoiceItem, invoice_item_repository.find_by_id(343)
+
+    refute nil, invoice_item_repository.find_by_id(343343243242)
+  end
+
+  def test_it_can_find_all_by_item_id
+    assert_instance_of InvoiceItem, invoice_item_repository.find_all_by_item_id(263563764).sample
+
+    refute nil, invoice_item_repository.find_by_id(343343243244)
+
+    assert_equal 2, invoice_item_repository.find_all_by_item_id(263563764).count
+  end
+
+  def test_it_can_find_all_by_invoice_id
+    assert_instance_of InvoiceItem, invoice_item_repository.find_all_by_invoice_id(14).sample
+
+    refute nil, invoice_item_repository.find_by_id(343343243243)
+
+    assert_equal 8, invoice_item_repository.find_all_by_invoice_id(14).count
+
+    #- returns either [] or one or more matches which have a matching invoice ID
   end
 
 end
--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -1,8 +1,7 @@
-# require_relative 'test_helper'
-require 'minitest/autorun'
+require_relative 'test_helper'
 require_relative './../lib/invoice_item'
-class InvoiceItemTest < Minitest::Test
 
+class InvoiceItemTest < Minitest::Test
 
   attr_reader :se, :invoice_item
 
@@ -14,20 +13,24 @@ class InvoiceItemTest < Minitest::Test
     assert_instance_of InvoiceItem, invoice_item
   end
 
-  def test_it_returns_the_invoice_item_id
+  def test_it_returns_the_invoiceitem_id
     assert_equal 1, invoice_item.id
   end
 
-  def test_it_returns_the_invoice_item_id
+  def test_it_returns_the_item_id
     assert_equal 263519844, invoice_item.item_id
   end
 
-  def test_it_returns_the_invoice_item_id
+  def test_it_returns_the_invoice_id
     assert_equal 1, invoice_item.invoice_id
   end
 
   def test_it_returns_the_quantity
     assert_equal 5, invoice_item.quantity
+  end
+
+  def test_it_returns_the_unit_price
+    assert_equal 13635, invoice_item.unit_price
   end
 
   def test_it_returns_the_date_created
@@ -38,7 +41,7 @@ class InvoiceItemTest < Minitest::Test
     assert_equal Time.parse("2012-03-27 14:54:09 UTC"), invoice_item.updated_at
   end
 
-  def test_it_can_return_unit_price
+  def test_it_can_return_unit_price_in_dollars
     assert_instance_of BigDecimal, invoice_item.unit_price_to_dollars
   end
 end
