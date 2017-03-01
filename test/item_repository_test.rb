a7174026b2d8fc6248a6c5e68f29fbe694d14e56
Sam Landfried <samlandfried@gmail.com>
2017-02-28 21:28:13 -0700

    Clean up ItemRepo

--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -1,10 +1,10 @@
 require_relative 'test_helper'
 
 class ItemRepositoryTest < Minitest::Test
-  attr_reader :se, :item_repository
+
+  attr_reader :item_repository
 
   def setup
-    @se = $sales_engine
     @item_repository = ItemRepository.from_csv('./test/fixtures/items_truncated.csv')
   end
 
@@ -13,41 +13,52 @@ class ItemRepositoryTest < Minitest::Test
   end
 
   def test_it_returns_all_known_item_instances
-    assert_equal 227, item_repository.all.length
+    assert_equal 227, item_repository.all.count
   end
 
   def test_it_finds_an_item_by_id
-    assert_instance_of Item, item_repository.find_by_id(263396209)
+    assert_equal item_repository.all.first, item_repository.find_by_id(263543344)
   end
 
   def test_it_finds_an_item_by_name
-    assert_instance_of Item, item_repository.find_by_name("Vogue Paris Original Givenchy 2307")
+    vogue_item = item_repository.find_by_id(263396209)
 
-    refute nil, item_repository.find_by_name("vogue")
+    assert_equal vogue_item, item_repository.find_by_name("Vogue Paris Original Givenchy 2307")
+    refute item_repository.find_by_name("NOTANAME")
   end
 
   def test_it_finds_an_item_with_description_fragment
-    assert_equal [], item_repository.find_all_with_description("fast cars")
+    wizard_items = item_repository.find_all_with_description("wizard")
 
-    assert_instance_of Item, item_repository.find_all_with_description("wizard").sample
+    assert_equal 3, wizard_items.count
+    assert_instance_of Item, wizard_items.sample
+    assert_empty item_repository.find_all_with_description("BADDESCRIPTION")
   end
 
   def test_it_finds_all_items_by_supplied_price
-    assert_equal 12, item_repository.find_all_by_price(20).count
+    items = item_repository.find_all_by_price(20)
+    item = items.sample
 
-    assert_instance_of Item, item_repository.find_all_by_price(20).sample
+    assert_equal 12, items.count
+    assert_instance_of Item, item
+    assert_equal 20, item.unit_price
   end
 
   def test_it_finds_all_items_in_a_supplied_range
-    assert_equal 58, item_repository.find_all_by_price_in_range(15..25).count
+    items_in_range = item_repository.find_all_by_price_in_range(50..60)
+    item = items_in_range.sample
+    price = item.unit_price
 
-    assert_instance_of Item, item_repository.find_all_by_price_in_range(50..100).sample
+    assert_equal 12, items_in_range.count
+    assert_instance_of Item, item
+    assert price >= 50 && price <= 60
   end
 
   def test_it_finds_all_items_by_merchant_id
-    assert_equal 3, item_repository.find_all_by_merchant_id(12334105).count
+    items = item_repository.find_all_by_merchant_id(12334105)
+    item = items.sample
 
-    assert_instance_of Item, item_repository.find_all_by_merchant_id(12334105).sample
+    assert_equal 3, items.count
+    assert_instance_of Item, item
   end
-
 end
