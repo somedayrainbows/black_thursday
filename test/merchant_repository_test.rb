f07673d54b21f4da8ea6f6037ebc37ecf13245d9
Sam Landfried <samlandfried@gmail.com>
2017-02-28 20:41:58 -0700

    Clean up MerchRepoTest

--- test/merchant_repository_test.rb
+++ test/merchant_repository_test.rb
@@ -1,10 +1,9 @@
 require_relative 'test_helper'
 
 class MerchantRepositoryTest < Minitest::Test
-  attr_reader :se, :merchant_repository
+  attr_reader :merchant_repository
 
   def setup
-    @se = $sales_engine
     @merchant_repository = MerchantRepository.from_csv('./test/fixtures/merchants_truncated.csv')
   end
 
@@ -16,21 +15,18 @@ class MerchantRepositoryTest < Minitest::Test
     assert_equal 89, merchant_repository.all.count
   end
 
-  def test_it_finds_an_merchant_by_id
+  def test_it_finds_a_merchant_by_id
     assert_instance_of Merchant, merchant_repository.find_by_id(12334861)
-
-    refute nil, merchant_repository.find_by_id(9823722)
+    refute merchant_repository.find_by_id(9823722)
   end
 
-  def test_it_finds_an_merchant_by_name
+  def test_it_finds_a_merchant_by_name
     assert_instance_of Merchant, merchant_repository.find_by_name("SamsMagicShop")
-
-    refute nil, merchant_repository.find_by_name("magic")
+    refute merchant_repository.find_by_name("magic")
   end
 
   def test_it_finds_all_merchants_by_name
     assert_equal 3, merchant_repository.find_all_by_name("the").count
-
     assert_instance_of Merchant, merchant_repository.find_all_by_name("sam").sample
   end
 end
