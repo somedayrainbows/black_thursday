04b7f8092f821f48d53653edfcfd89014ba9253c
Sam Landfried <samlandfried@gmail.com>
2017-02-28 20:09:21 -0700

    Clean up InItRepo

--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -1,13 +1,10 @@
-require_relative './../lib/invoice_item_repository'
-require 'csv'
 require_relative 'test_helper'
-require 'pry'
+require_relative './../lib/invoice_item_repository'
 
 class InvoiceItemRepositoryTest < Minitest::Test
-  attr_reader :se, :invoice_item_repository
+  attr_reader :invoice_item_repository
 
   def setup
-    @se = $sales_engine
     @invoice_item_repository = InvoiceItemRepository.from_csv('./test/fixtures/invoice_items_truncated.csv')
   end
 
@@ -16,33 +13,22 @@ class InvoiceItemRepositoryTest < Minitest::Test
   end
 
   def test_it_returns_an_array_of_invoice_items
-    assert_equal 467, se.invoice_items.all.count
-    assert Array, invoice_item_repository.all
+    assert_equal 467, invoice_item_repository .all.count
     assert_instance_of InvoiceItem, invoice_item_repository.all.sample
   end
 
-  def test_it_can_find_itself_with_its_id
+  def test_it_can_find_an_item_with_id
     assert_instance_of InvoiceItem, invoice_item_repository.find_by_id(343)
-
-    refute nil, invoice_item_repository.find_by_id(343343243242)
   end
 
   def test_it_can_find_all_by_item_id
-    assert_instance_of InvoiceItem, invoice_item_repository.find_all_by_item_id(263563764).sample
-
-    refute nil, invoice_item_repository.find_by_id(343343243244)
-
     assert_equal 2, invoice_item_repository.find_all_by_item_id(263563764).count
+    assert_instance_of InvoiceItem, invoice_item_repository.find_all_by_item_id(263563764).sample
   end
 
   def test_it_can_find_all_by_invoice_id
-    assert_instance_of InvoiceItem, invoice_item_repository.find_all_by_invoice_id(14).sample
-
-    refute nil, invoice_item_repository.find_by_id(343343243243)
-
     assert_equal 8, invoice_item_repository.find_all_by_invoice_id(14).count
-
-    #- returns either [] or one or more matches which have a matching invoice ID
+    assert_instance_of InvoiceItem, invoice_item_repository.find_all_by_invoice_id(14).sample
   end
 
 end
